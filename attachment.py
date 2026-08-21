e393bc350edc063d9a1896f978a3799767822140
Albert Cervera i Areny <albert@nan-tic.com>
2026-08-21 23:47:24 +0200

    Rename edit_url to collabora_url and use a button to open Collabora.

--- attachment.py
+++ attachment.py
@@ -1,4 +1,4 @@
-from trytond.model import fields
+from trytond.model import fields, ModelView
 from trytond.pool import PoolMeta
 from trytond.pyson import Bool, Eval
 
@@ -8,14 +8,24 @@ from .editor import get_editor_url, is_editable_filename
 class Attachment(metaclass=PoolMeta):
     __name__ = 'ir.attachment'
 
-    edit_url = fields.Function(
-        fields.Char('Edit URL', states={
-                'invisible': ~Bool(Eval('edit_url')),
+    collabora_url = fields.Function(
+        fields.Char('Open', states={
+                'invisible': ~Bool(Eval('collabora_url')),
                 }),
-        'get_edit_url')
+        'get_collabora_url')
 
     @classmethod
-    def get_edit_url(cls, attachments, name):
+    def __setup__(cls):
+        super().__setup__()
+        cls._buttons.update({
+                'open': {
+                    'invisible': ~Bool(Eval('collabora_url')),
+                    'depends': ['collabora_url'],
+                    },
+                })
+
+    @classmethod
+    def get_collabora_url(cls, attachments, name):
         return {
             attachment.id: (
                 get_editor_url(cls.__name__, attachment.id, 'data',
@@ -23,3 +33,12 @@ class Attachment(metaclass=PoolMeta):
                 if attachment.type == 'data'
                 and is_editable_filename(attachment.name) else None)
             for attachment in attachments}
+
+    @classmethod
+    @ModelView.button
+    def open(cls, attachments):
+        attachment, = attachments
+        return {
+            'type': 'ir.action.url',
+            'url': attachment.collabora_url,
+            }
