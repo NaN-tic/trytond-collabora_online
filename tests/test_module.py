4be37825eb0719f8a38893640cf47b9fb4f2dbb3
Albert Cervera i Areny <albert@nan-tic.com>
2026-08-22 00:03:02 +0200

    Add open button to papyrus' attachment list view.

--- tests/test_module.py
+++ tests/test_module.py
@@ -4,6 +4,7 @@ from trytond.tests.test_tryton import ModuleTestCase
 class CollaboraOnlineTestCase(ModuleTestCase):
     'Test Collabora Online module'
     module = 'collabora_online'
+    extras = ['papyrus']
 
 
 del ModuleTestCase
